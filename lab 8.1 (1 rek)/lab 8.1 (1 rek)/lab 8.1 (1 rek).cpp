5afd4a32d7f05888383e4d59fbef11146aae842f
Iryna Prendkovych <iryna.prendkovych.itis.2020@lpnu.ua>
2020-11-23 21:23:00 +0200

    lab 8.1 rek

---
+++ lab 8.1 (1 rek)/lab 8.1 (1 rek)/lab 8.1 (1 rek).cpp
@@ -0,0 +1,67 @@
+﻿// lab 8.1 (1 rek).cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+int Count(char* str, int i)
+{
+	if (strlen(str) < 3)
+		return 0;
+	if (str[i + 1] != 0)
+		if (str[i - 1] == 'n' && str[i + 1] == 'o')
+			return 1 + Count(str, i + 1);
+		else
+			return Count(str, i + 1);
+	else
+		return 0;
+}
+char* Change(char* dest, const char* str, char* t, int i)
+{
+	if (str[i + 2] != 0)
+	{
+		if (str[i] == 'n' && str[i + 2] == 'n')
+		{
+			strcat(t, "**");
+			return Change(dest, str, t + 4, i + 3);
+		}
+		else
+		{
+			*t++ = str[i++];
+			*t = '\0';
+			return Change(dest, str, t, i);
+		}
+	}
+	else
+	{
+		*t++ = str[i++];
+		*t++ = str[i++];
+		*t = '\0';
+		return dest;
+	}
+}
+int main()
+{
+	char str[101];
+	cout << "Enter string:" << endl;
+	cin.getline(str, 100);
+	cout << "String contained " << Count(str, 1) << " groups of 'n o'" << endl;
+	char* dest1 = new char[151];
+	dest1[0] = '\0';
+	char* dest2;
+	dest2 = Change(dest1, str, dest1, 0);
+	cout << "Modified string (param) : " << dest1 << endl;
+	cout << "Modified string (result): " << dest2 << endl;
+	return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
